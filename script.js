68bc9f59e1601b659c7596996c43a66bb36cbba7
migo1 <ertmigot@gmail.com>
2023-05-15 16:47:43 +0300

    add js linter

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-
-const hamburger = document.querySelector(".hamburger");
-const mobileMenu = document.querySelector(".mobile-nav");
-const closeBtn = document.querySelector(".mobile-cancel");
-const menuItems = document.querySelectorAll(".mobile-item-nav");
-
-
-hamburger.addEventListener("click", toggleMenu);
-
-closeBtn.addEventListener("click", toggleMenu);
-
-window.addEventListener("click", function (e) {
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
+const closeBtn = document.querySelector('.mobile-cancel');
+const menuItems = document.querySelectorAll('.mobile-item-nav');
+function toggleMenu() {
+  if (mobileMenu.style.display === 'block') {
+    mobileMenu.style.display = 'none';
+  } else {
+    mobileMenu.style.display = 'block';
+  }
+}
+hamburger.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+window.addEventListener('click', (e) => {
   if (e.target === mobileMenu) {
-    mobileMenu.style.display = "none";
+    mobileMenu.style.display = 'none';
   }
 });
-
-for (i = 0; i < menuItems.length; i++){
-    menuItems[i].addEventListener("click", toggleMenu);
-}
-
-function toggleMenu() {
-  if (mobileMenu.style.display === "block") {
-    mobileMenu.style.display = "none";
-  } else {
-    mobileMenu.style.display = "block";
-  }
+for (let i = 0; i < menuItems.length; i += 1) {
+  menuItems[i].addEventListener('click', toggleMenu);
 }
