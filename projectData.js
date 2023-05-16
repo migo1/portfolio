9f9bfaf6c03f0d97fb7b722deb1a5a1812270f0f
migo1 <ertmigot@gmail.com>
2023-05-16 10:07:06 +0300

    add-card-from-js-file

---
+++ projectData.js
@@ -0,0 +1,43 @@
+const projectData = [
+  {
+    id: 0,
+    name: 'Tonic',
+    description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work1.png',
+    image_desk: 'images/desktop/work1.png',
+    tech_stack: ['html', 'css', 'js', 'Ruby', 'Bootstrap'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+  },
+  {
+    id: 1,
+    name: 'Tonic',
+    description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work2.png',
+    image_desk: 'images/desktop/work2.png',
+    tech_stack: ['html', 'css', 'js', 'Ruby', 'Bootstrap'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+  },
+  {
+    id: 2,
+    name: 'Tonic',
+    description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work3.png',
+    image_desk: 'images/desktop/work3.png',
+    tech_stack: ['html', 'css', 'js', 'Ruby', 'Bootstrap'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+  },
+];
+
+export default projectData;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import projectData from './projectData.js';
+
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('.mobile-cancel');
@@ -18,4 +20,44 @@ window.addEventListener('click', (e) => {
 });
 for (let i = 0; i < menuItems.length; i += 1) {
   menuItems[i].addEventListener('click', toggleMenu);
+}
+const project = document.getElementById('projects');
+for (let i = 0; i < projectData.length; i += 1) {
+  const cardDiv = document.createElement('div');
+  if (projectData[i].id % 2 === 1) {
+    cardDiv.classList.add('card', 'card-inverse');
+  } else {
+    cardDiv.classList.add('card');
+  }
+  const imageDiv = document.createElement('div');
+  imageDiv.classList.add('image');
+  const picture = document.createElement('picture');
+  const source1 = document.createElement('source');
+  source1.setAttribute('media', '(max-width: 767px)');
+  source1.setAttribute('srcset', projectData[i].image_mobile);
+  const source2 = document.createElement('source');
+  source2.setAttribute('media', '(min-width: 768px)');
+  source2.setAttribute('srcset', projectData[i].image_desk);
+  const img = document.createElement('img');
+  img.classList.add('card-image');
+  img.setAttribute('src', projectData[i].image_mobile);
+  img.setAttribute('alt', `mobile work${projectData[i].id} image`);
+  picture.appendChild(source1);
+  picture.appendChild(source2);
+  picture.appendChild(img);
+  imageDiv.appendChild(picture);
+  cardDiv.appendChild(imageDiv);
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card-content');
+  const cardTitle = document.createElement('h3');
+  cardTitle.classList.add('card-title');
+  cardTitle.innerText = projectData[i].title;
+  const projectExperience = document.createElement('ul');
+  projectExperience.classList.add('project-experience');
+  for (let j = 0; j < project[i].project_experience.length; i += 1) {
+    if (project[i].project_experience[0]) {
+      const firstList
+    }
+  }
+  project.appendChild(cardDiv);
 }
