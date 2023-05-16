11d1b67169394fd67c5b3447964fdf687ebabe02
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-05-16 16:07:36 +0700

    Removing portfolio card in index.html

--- projectData.js
+++ projectData.js
@@ -38,6 +38,19 @@ const projectData = [
         'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
     link_repo: 'https://github.com/migo1/portfolio',
   },
+  {
+    id: 3,
+    name: 'Tonic',
+    summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work3.png',
+    image_desk: 'images/desktop/work3.png',
+    tech_stack: ['html', 'css', 'js'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+  },
 ];
 
 export default projectData;
