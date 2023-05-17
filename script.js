42238267769b1c1dd3e78637ed6f50212390cb92
migo1 <ertmigot@gmail.com>
2023-05-17 14:45:38 +0300

    add-projects-data-to-script.js

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
-import projectData from './projectData.js';
+const projectData = [
+  {
+    id: 0,
+    name: 'Tonic',
+    summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work1.png',
+    image_desk: 'images/desktop/work1.png',
+    tech_stack: ['html', 'css', 'java script'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+    modal_data: {
+      languages: ['html', 'css', 'java script', 'Ruby', 'Bootstrap'],
+      desc: 'Lorem Ipsum is simply dummy text of the printing industry.unknown printer took a galley of typeand scrambled it to make a type. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    },
+  },
+  {
+    id: 1,
+    name: 'Tonic',
+    summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work2.png',
+    image_desk: 'images/desktop/work2.png',
+    tech_stack: ['html', 'css', 'java script'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+    modal_data: {
+      languages: ['html', 'css', 'java script', 'Ruby', 'Bootstrap'],
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy textever since the 1500s, when an unknown printer took a galley of typeand scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    },
+  },
+  {
+    id: 2,
+    name: 'Tonic',
+    summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work3.png',
+    image_desk: 'images/desktop/work3.png',
+    tech_stack: ['html', 'css', 'java script'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+    modal_data: {
+      languages: ['html', 'css', 'java script', 'Ruby', 'Bootstrap'],
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy textever since the 1500s, when an unknown printer took a galley of typeand scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    },
+  },
+  {
+    id: 3,
+    name: 'Tonic',
+    summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required .',
+    image_mobile: 'images/work3.png',
+    image_desk: 'images/desktop/work3.png',
+    tech_stack: ['html', 'css', 'java script'],
+    project_experience: ['CANOPY', 'Back End Dev', 2015],
+    link_live:
+        'https://www.youtube.com/watch?v=9EcjWd-O4jI&ab_channel=TechnotronicVEVO',
+    link_repo: 'https://github.com/migo1/portfolio',
+    modal_data: {
+      languages: ['html', 'css', 'java script', 'Ruby', 'Bootstrap'],
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy textever since the 1500s, when an unknown printer took a galley of typeand scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    },
+  },
+];
 
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav');
